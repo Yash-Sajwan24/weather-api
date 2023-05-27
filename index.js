2a22877b787c7b1b3f3e8017b4351b4c33e693fb
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-05-28 04:23:19 +0530

    added an input city field

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const https = require('https');
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Africa&appid=74da1b2c37607c129cdfb80a15d6fc08&units=metric";
-    https.get(url, function(response){
-        response.on('data', function(data){
-            const weatherData = JSON.parse(data);
-            const description = weatherData.weather[0].description;
-            const temp = weatherData.main.temp;
-            const visibility = weatherData.visibility;
-            const icon = weatherData.weather[0].icon;
-            const iconURL = 'https://openweathermap.org/img/wn/'+icon+'@2x.png';
-
-            res.write("<h1> the weather is "+description +"</h1>");
-            res.write("<h1> the temprature is "+temp +"</h1>");
-            res.write("<h1> the visibility is "+visibility +"</h1>");
-            res.write('<img src="'+iconURL+'">');
-            res.send();
-
-        })
-    })
+    res.sendFile(__dirname + "/index.html");
 })
+
+app.post('/', function(req, res){
+    const enteredCity = req.body.inputText;
+    res.send(enteredCity);
+})
+
+// let city = "Africa";
+//     let appid = "74da1b2c37607c129cdfb80a15d6fc08";
+//     let unit = 'metric';
+//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&units=${unit}`;
+//     https.get(url, function(response){
+//         response.on('data', function(data){
+//             const weatherData = JSON.parse(data);
+//             const description = weatherData.weather[0].description;
+//             const temp = weatherData.main.temp;
+//             const visibility = weatherData.visibility;
+//             const icon = weatherData.weather[0].icon;
+//             const iconURL = 'https://openweathermap.org/img/wn/'+icon+'@2x.png';
+
+//             res.write("<h1> the weather is "+description +"</h1>");
+//             res.write("<h1> the temprature is "+temp +"</h1>");
+//             res.write("<h1> the visibility is "+visibility +"</h1>");
+//             res.write('<img src="'+iconURL+'">');
+//             res.send();
+//         })
+//     })
 
 
 
